64b97263ce093ee1c94608e82ea77c9c43d711d5
gomona157 <131306368+gomona157@users.noreply.github.com>
2023-05-26 16:20:56 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("Olá Mundo")
+  var nome = "Julia"
+  var nascimento = 2006
+
+  println(nome)          // exibir o nome
+  println(nascimento)     // exibir o nascimento
+
+val myNum: Int = 5                // Int
+val myDoubleNum: Double = 5.99    // Double
+val myLetter: Char = 'J'          // Char
+val myBoolean: Boolean = true     // Boolean
+val myText: String = "Olá"      // String
+
+println (myNum)
+println (myDoubleNum)
+println (myLetter)
+println (myBoolean)
+println (myText)
+}
